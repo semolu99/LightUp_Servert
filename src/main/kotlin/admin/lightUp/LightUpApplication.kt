e32f2276ac4851b17457f91ba3366fa8cdaf3027
JaeSung <barbo9510@icloud.com>
2025-07-08 03:39:33 +0900

    첫 설정

---
+++ src/main/kotlin/admin/lightUp/LightUpApplication.kt
@@ -0,0 +1,11 @@
+package admin.lightUp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LightUpApplication
+
+fun main(args: Array<String>) {
+	runApplication<LightUpApplication>(*args)
+}
---
+++ src/test/kotlin/admin/lightUp/LightUpApplicationTests.kt
@@ -0,0 +1,13 @@
+package admin.lightUp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LightUpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
